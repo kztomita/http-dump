ea18b53c2adb0db0d62f0b76f47399bf572f13e7
Kazuyoshi Tomita <kztomita@bit-hive.com>
2019-11-06 22:51:04 +0900

    127文字以上のHPACKエンコードに対応

--- hpack_encoder.cc
+++ hpack_encoder.cc
@@ -1,32 +1,48 @@
 #include "hpack_encoder.h"
 #include <cstddef>
+#include <cstdint>
 #include <cstring>
 #include <stdexcept>
 
 namespace hpack {
 
+namespace {
+void encode_integer(uint8_t src, unsigned int i, std::size_t n, http2_frame::payload_type& payload) {
+  // 2 ^ n - 1
+  unsigned int mask = (1 << n) - 1;
+
+  if (i < mask) {
+    payload.push_back((src & ~mask) + (i & mask));
+  } else {
+    payload.push_back(src | mask);
+    i -= mask;
+    while (i >= 128) {
+      payload.push_back(i % 128 + 128);
+      i /= 128;
+    }
+    payload.push_back(i);
+  }
+}
+
+}  // unnamed namespace
+
 http2_frame::payload_type hpack_encoder::encode(const header_list& headers) {
   http2_frame::payload_type payload;
 
   for (const auto& h : headers) {
-    if (h.first.size() >= 127) {
-      throw std::runtime_error("header name is too long. Supports less than 127 characters.");
-    }
-    if (h.second.size() >= 127) {
-      throw std::runtime_error("header value is too long. Supports less than 127 characters.");
-    }
-    std::size_t offset = payload.size();
-    payload.resize(payload.size() + 3 + h.first.size() + h.second.size());
-    unsigned char* p = &payload[offset];
-    *p = 0x00;  // without Indexing
-    p++;
-    *p = h.first.size();
-    p++;
-    std::memcpy(p, &h.first[0], h.first.size());
-    p += h.first.size();
-    *p = h.second.size();
-    p++;
-    std::memcpy(p, &h.second[0], h.second.size());
+    payload.push_back(0x00);    // Literal Header Field without Indexing
+
+    // Name Length
+    encode_integer(0x00, h.first.size(), 7, payload);
+
+    // Name String
+    std::copy(h.first.begin(), h.first.end(), std::back_inserter(payload));
+
+    // Value Length
+    encode_integer(0x00, h.second.size(), 7, payload);
+
+    // Value String
+    std::copy(h.second.begin(), h.second.end(), std::back_inserter(payload));
   }
 
   return payload;
